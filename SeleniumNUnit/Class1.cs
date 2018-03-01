27418ae6e46c2b603b9a546185636d9d57ede729
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-03-01 19:37:23 +0530

    first commit

---
+++ SeleniumNUnit/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+
+namespace SeleniumNUnit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            //Launch the ToolsQA Website
+            driver.Url = "http://www.demoqa.com";
+
+            // Storing Title name in String variable
+            String Title = driver.Title;
+
+            // Storing Title length in Int variable
+            int TitleLength = driver.Title.Length;
+
+            // Printing Title name on Console
+            Console.WriteLine("Title of the page is : " + Title);
+
+            // Printing Title length on console
+            Console.WriteLine("Length of the Title is : " + TitleLength);
+
+            // Storing URL in String variable
+            String PageURL = driver.Url;
+
+            // Storing URL length in Int variable
+            int URLLength = PageURL.Length;
+
+            // Printing URL on Console
+            Console.WriteLine("URL of the page is : " + PageURL);
+
+            // Printing URL length on console
+            Console.WriteLine("Length of the URL is : " + URLLength);
+
+            // Storing Page Source in String variable
+            String PageSource = driver.PageSource;
+
+            // Storing Page Source length in Int variable
+            int PageSourceLength = driver.PageSource.Length;
+
+            // Printing Page Source on console
+            Console.WriteLine("Page Source of the page is : " + PageSource);
+
+            // Printing Page SOurce length on console
+            Console.WriteLine("Length of the Page Source is : " + PageSourceLength);
+
+            //Closing browser
+            driver.Quit();
+        }
+       
+    }
+}
