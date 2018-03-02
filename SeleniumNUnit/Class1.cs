ec6a8f8b8c70d752fc1c5ffc14987afab9d10883
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-03-02 13:18:25 +0530

    added

--- SeleniumNUnit/Class1.cs
+++ SeleniumNUnit/Class1.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
+using OpenQA.Selenium.Remote;
 
 
 namespace SeleniumNUnit
@@ -14,53 +15,75 @@ namespace SeleniumNUnit
     [TestFixture]
     public class Class1
     {
+       static  IWebDriver driver;
+
+        [TestFixtureSetUp]
+        public void fixSetup()
+        {
+          
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            ChromeOptions options= new ChromeOptions();
+           // options.AddArguments("--incognito");  //to open chrome browser in incognito mode
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(30); //will wait for  30 seconds before throw the exception
+            
+        }
         [Test]
         public void Test1()
         {
-            IWebDriver driver = new ChromeDriver();
+            
 
             //Launch the ToolsQA Website
             driver.Url = "http://www.demoqa.com";
 
             // Storing Title name in String variable
-            String Title = driver.Title;
+            String title = driver.Title;
 
             // Storing Title length in Int variable
-            int TitleLength = driver.Title.Length;
+            int titleLength = driver.Title.Length;
 
             // Printing Title name on Console
-            Console.WriteLine("Title of the page is : " + Title);
+            Console.WriteLine("Title of the page is : " + title);
 
             // Printing Title length on console
-            Console.WriteLine("Length of the Title is : " + TitleLength);
+            Console.WriteLine("Length of the Title is : " + titleLength);
 
             // Storing URL in String variable
             String PageURL = driver.Url;
 
             // Storing URL length in Int variable
-            int URLLength = PageURL.Length;
+            int urlLength = PageURL.Length;
 
             // Printing URL on Console
             Console.WriteLine("URL of the page is : " + PageURL);
 
             // Printing URL length on console
-            Console.WriteLine("Length of the URL is : " + URLLength);
+            Console.WriteLine("Length of the URL is : " + urlLength);
 
             // Storing Page Source in String variable
-            String PageSource = driver.PageSource;
+            string PageSource = driver.PageSource;
 
             // Storing Page Source length in Int variable
-            int PageSourceLength = driver.PageSource.Length;
+            int pageSourceLength = driver.PageSource.Length;
 
             // Printing Page Source on console
             Console.WriteLine("Page Source of the page is : " + PageSource);
 
             // Printing Page SOurce length on console
-            Console.WriteLine("Length of the Page Source is : " + PageSourceLength);
+            Console.WriteLine("Length of the Page Source is : " + pageSourceLength);
 
             //Closing browser
             driver.Quit();
         }
+
+
+
+
        
     }
 }
