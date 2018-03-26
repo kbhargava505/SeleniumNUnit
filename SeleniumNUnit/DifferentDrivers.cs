7d3c04898efee98a1684a78bdd43d562b59a6c3c
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-03-26 08:25:44 +0530

    updated on 25-3-18

---
+++ SeleniumNUnit/DifferentDrivers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// we need to use many drivers as possible
+    //AndroidDriver,
+    //ChromeDriver,
+    //EventFiringWebDriver,
+    //FirefoxDriver,
+    //HtmlUnitDriver,
+    //InternetExplorerDriver,
+    //IPhoneDriver,
+    //IPhoneSimulatorDriver,
+    //RemoteWebDriver.
+namespace SeleniumNUnit
+{
+    class DifferentDrivers
+    {
+    }
+}
--- SeleniumNUnit/Waits.cs
+++ SeleniumNUnit/Waits.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
@@ -9,13 +14,13 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-
 namespace SeleniumNUnit
 {
     [TestFixture]
-    class BrowserNavigations
+    class Waits
     {
         private IWebDriver driver;
+        private WebDriverWait check;
 
         [SetUp]
         public void SetupForEveryTest()
@@ -23,26 +28,30 @@ namespace SeleniumNUnit
             driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             driver.Manage().Window.Maximize();
+            check = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
-        [Test, Description("Navigate to URL")]
-        public void Browser_BasicNavigations()
+        [Test, Description("using webDriveWait")]
+        public void TypesofWaits()
         {
             //Navigates the browser to given URL
-            driver.Navigate().GoToUrl("http://google.com"); 
+            driver.Navigate().GoToUrl("http://google.com");
+            check.Until(ExpectedConditions.ElementExists(By.Name("btnK"))); 
             Assert.True(driver.Url.Contains("https://www.google"), "Validate Navigation to google url");
 
             driver.Url = "http://facebook.com"; //Almost same as Navigate().GoToUrl() method
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.MaxValue;//Implicit wait //wait till the time span max value(2.5hrs)
             Assert.AreEqual(driver.Url, "https://www.facebook.com/", "Validate Navigation to facebook url");
 
             driver.Navigate().Back(); //Clicks on back button of browser, navigates back to previously visited url (browser history)
+            Thread.Sleep(1000);     //explicit wait
             Assert.True(driver.Url.Contains("https://www.google"), "Validate Navigation to google url");
 
             driver.Navigate().Forward();//Clicks on forward button of browser, navigates as per browseer history
-
             Assert.AreEqual(driver.Url, "https://www.facebook.com/", "Validate Navigation to facebook url");
 
             driver.Navigate().Refresh(); //Reloads the current page
+            IWebElement CreateAccount = check.Until(ExpectedConditions.ElementToBeClickable(By.Name("websubmit")));
             Assert.AreEqual(driver.Url, "https://www.facebook.com/", "Validate Navigation to facebook url");
 
             driver.Manage().Cookies.DeleteAllCookies();
