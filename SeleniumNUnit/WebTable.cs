938278fb8f495b600c66dbaf03b29c04a66ead89
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-07-24 18:48:11 +0530

    webtable

--- SeleniumNUnit/WebTable.cs
+++ SeleniumNUnit/WebTable.cs
@@ -12,8 +12,20 @@ namespace SeleniumNUnit
 {
   [TestFixture]
     class WebTable
-    {
-        
+  {
+      private IWebDriver Driver;
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+          Driver  = new ChromeDriver();
 
+        }
+        [Test]
+        public void TableHeader()
+        {
+            Driver.Url = "http://toolsqa.com/automation-practice-table/";
+           string a=Driver.FindElement(By.XPath("//table[@class='tsc_table_s13']/thead/tr/th[last()]")).Text;   //use last method
+            Console.WriteLine(a);
+        }
     }
 }
