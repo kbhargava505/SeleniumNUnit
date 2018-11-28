c3c7e439a1d3ed379b5e63cce8dffeae709a2c7b
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-11-28 11:09:40 +0530

    WebTable Xpath test cases

--- SeleniumNUnit/WebTable.cs
+++ SeleniumNUnit/WebTable.cs
@@ -21,11 +21,33 @@ namespace SeleniumNUnit
 
         }
         [Test]
-        public void TableHeader()
+        public void UsingMethods()
         {
             Driver.Url = "http://toolsqa.com/automation-practice-table/";
            string a=Driver.FindElement(By.XPath("//table[@class='tsc_table_s13']/thead/tr/th[last()]")).Text;   //use last method
             Console.WriteLine(a);
+            a = Driver.FindElement(By.XPath("//td[contains(text(),'60')]")).Text;
+            Console.WriteLine(a);
         }
+        [Test]
+        public void PrecedingAndFollowingElements()
+        {
+            Driver.Url = "http://toolsqa.com/automation-practice-table/";
+            IWebElement b = Driver.FindElement(By.XPath("//td[text()='Dubai']/following-sibling::td/a")); //clicking on details link which is after Dubai text
+            b.Click();
+            Driver.FindElement(By.XPath("//th[text()='Burj Khalifa']/following-sibling::td[last()]")).Click(); //last item in the row            
+            string c = Driver.FindElement(By.XPath("//td[following-sibling::td[text()='Mecca']]")).Text; //it gives the values for which Mecca is following sibling
+            Console.WriteLine(c);
+            c = Driver.FindElement(By.XPath("//td[preceding-sibling::td[text()='Mecca']]/a")).Text; //gives a for which parent is preceding sibling for Mecca
+            Console.WriteLine(c);
+            Console.WriteLine(Driver.FindElement(By.XPath("//td[preceding-sibling::td[text()='601m'] and following-sibling::td='2']")).Text);//finding text between 601 and 2
+            Console.WriteLine(Driver.FindElement(By.XPath("//td[preceding-sibling::td[text()='UAE']]")).Text);//getting values for which UAE is preceding sibling, it will be in circular form
+        }
+        [TestFixtureTearDown]
+        public void Teardown()
+        {
+            Driver.Quit();
+        }
+
     }
 }
